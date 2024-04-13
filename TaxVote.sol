e12065138bcc3c24132afad2617c0280dc9e8c8b
resilience-me <emailsareobsolete@gmail.com>
2024-04-13 13:18:25 +0200

    Update TaxVote.sol

--- TaxVote.sol
+++ TaxVote.sol
@@ -4,7 +4,7 @@ contract BitPeople {
 }
 contract PAN { function setTaxRate(uint tax) external {} }
 contract Schedule {
-    uint constant public genesis = 1710568800;
+    uint constant public genesis = 1712988000;
     uint constant public period = 4 weeks;
     function schedule() public view returns(uint) { return ((block.timestamp - genesis) / period); }
 }
