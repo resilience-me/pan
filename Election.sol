b73122e9dda6ab72ed404d99276f23b9dabccb17
resilience-me <emailsareobsolete@gmail.com>
2024-04-13 13:17:50 +0200

    Update Election.sol

--- Election.sol
+++ Election.sol
@@ -2,7 +2,7 @@ contract BitPeople { function proofOfUniqueHuman(uint t, address account) extern
 
 contract Schedule {
 
-    uint constant public genesis = 1710568800;
+    uint constant public genesis = 1712988000;
     uint constant public period = 4 weeks;
 
     function schedule() public view returns(uint) { return ((block.timestamp - genesis) / period); }
