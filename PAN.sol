06b99c28ce01156ed1124f90679118c9ea57aa30
resilience-me <emailsareobsolete@gmail.com>
2024-04-13 13:18:08 +0200

    Update PAN.sol

--- PAN.sol
+++ PAN.sol
@@ -4,7 +4,7 @@ contract BitPeople {
 }
 
 contract Schedule {
-    uint constant public genesis = 1710568800;
+    uint constant public genesis = 1712988000;
     uint constant public period = 4 weeks;
     function schedule() public view returns(uint) { return ((block.timestamp - genesis) / period); }
     function toSeconds(uint t) public pure returns (uint) { return genesis + t * period; }
