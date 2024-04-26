6bb177eefa368d0d8ec4c83941ef39f70c6fbed6
resilience-me <emailsareobsolete@gmail.com>
2024-04-26 14:40:59 +0200

    Update logo.js

--- website/logo.js
+++ website/logo.js
@@ -4,7 +4,7 @@ function adjustLogo() {
     var footerHeight = document.querySelector('footer').offsetHeight;
     var bodyStyles = window.getComputedStyle(document.body);
     var bodyPaddingVertical = parseInt(bodyStyles.paddingTop) + parseInt(bodyStyles.paddingBottom);
-    var logoPadding = 20;
+    var logoPadding = 60;
     var logoContainerHeight = windowHeight-bodyPaddingVertical-contentHeight-footerHeight-logoPadding;
     var bodyPaddingHorizontal = parseInt(bodyStyles.paddingLeft) + parseInt(bodyStyles.paddingRight);
     var logoContainerWidth = window.innerWidth-bodyPaddingHorizontal-logoPadding;
@@ -15,8 +15,8 @@ function adjustLogo() {
     var logoContainer = document.querySelector('.logoContainer');
     logoContainer.style.height = `${logoSize}px`;
     logoContainer.style.width = `${logoSize}px`;
-    var horizontalOffset = (logoContainerWidth - logoSize) / 2;
-    var verticalOffset = (logoContainerHeight - logoSize) / 2;
+    var horizontalOffset = logoPadding/2 + (logoContainerWidth - logoSize) / 2;
+    var verticalOffset = logoPadding/2 + (logoContainerHeight - logoSize) / 2;
     var logoImage = document.querySelector('.logoImage');
     logoImage.style.paddingLeft = `${horizontalOffset}px`;
     logoImage.style.paddingTop = `${verticalOffset}px`;
